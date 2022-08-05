6500f891ee86a0cc0d24bb2d9b3d313f1fd97060
Vetali1985 <geelycross1985@gmail.com>
2022-08-05 19:27:42 +0300

    05.08

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,17 @@
-const itemEl = document.querySelectorAll(`ul#categories.item`)
-console.log(itemEl)
+const itemEl = document.querySelectorAll(`li.item`)
+
+console.log('Number of categories:', itemEl.length)
+    
+
+const itemsCategories = document.querySelectorAll('li.item')
+
+itemsCategories.forEach(item => {
+
+    console.log(`Category: ${item.querySelector('h2').textContent}`);
+    console.log(`Elements: ${item.querySelectorAll('li').length}
+        `)
+})
+
+      
+      
+
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const task = document.querySelector(`#ingredients`)
+ ingredients.forEach(el => {
+ const ingEl = document.createElement(`li`)
+  ingEl.classList.add(`item`)
+  ingEl.textContent = (el)
+   task.append(ingEl)
+   
+})
+
+
+  
+  
+ 
+
+ 
+
+// console.log(rt)
+
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// };
+
+// numbers.forEach(logMessage);
+
+
